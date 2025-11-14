f87fa7d5eca07379b3a52e59084064bcbf6ebc29
Isaias Avila Rosales <avilaisaias0607@gmail.com>
2025-11-14 16:40:07 -0600

    v01
    Se crea la estructura del correo: con una clase que funge como centro de mando y integra el motodo de busqueta y escritura de XMLRPC y la creación y envio del correo.

---
+++ ingresos.py
@@ -0,0 +1,119 @@
+from dotenv import load_dotenv
+import os
+import xmlrpc.client
+from datetime import datetime
+
+
+class odoo_conection:
+
+    def __init__(self):
+        load_dotenv()
+
+    def start_odoo_connection(self):
+        # -- Odoo connection - Begin
+        self.common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(os.getenv('URL_ACCS')))
+
+        self.uid = self.common.authenticate(os.getenv('DB_LOC'), os.getenv('DB_USR'), os.getenv('DB_PASS'), {})
+
+        if not self.uid:
+            raise Exception("Error de autenticación en Odoo")
+
+        self.models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(os.getenv('URL_ACCS')))
+        # -- Odoo connection - Begin
+
+        # Save DataBase 
+        return self.models
+    
+    def close_odoo_connection(self):
+        self.common._ServerProxy__transport.close()
+        self.models._ServerProxy__transport.close()
+
+    
+    def get_ingresos(self, models):
+        domain =  [        
+            ['x_studio_stage_id','=',4],
+            ['x_studio_tipo_de_ingreso','=','ONE STOP'],
+            ['x_studio_correo_electronico','=','NO ENVIADO'],
+        ]
+
+        fields_list = ['x_name', 'x_studio_orden_de_reparacin','x_studio_producto', 'x_studio_numero_de_serie_interna','x_studio_modelo','x_studio_paqueteria',
+                  'create_date','x_studio_stage_id','x_studio_orden_general']
+        
+        fields = fields_list
+
+        datos = models.execute_kw(
+            os.getenv('DB_LOC'), 
+            self.uid, 
+            os.getenv('DB_PASS'),     
+            'x_ingreso_lb', 
+            'search_read', 
+            [domain],
+            {'fields': fields})
+        
+        return datos
+    
+    def write_ingreso(self,models,ids):
+
+
+        models.execute_kw(
+            os.getenv('DB_LOC'), 
+            self.uid, 
+            os.getenv('DB_PASS'),     
+            'x_ingreso_lb', 
+            'write',
+            [[ids], {'x_studio_correo_electronico': 'ENVIADO'}])
+
+        #for item in datos:
+        #    lista = []
+        #    url = f"https://agiotech.odoo.com//web#id={item.get('id')}&model=x_ingreso_lb&view_type=form"
+        #    lista.append(url)
+        #    lista.append(item.get('x_name'))
+        #    lista.append(item.get('x_studio_orden_de_reparacin',['',''])[1] if item.get('x_studio_orden_de_reparacin') else '')
+        #    lista.append(item.get('x_studio_producto',['',''])[1] if item.get('x_studio_producto') else '')
+        #    lista.append(item.get('x_studio_numero_de_serie_interna',['',''])[1] if item.get('x_studio_numero_de_serie_interna') else '')
+        #    lista.append(item.get('x_studio_modelo',['',''])[1] if item.get('x_studio_modelo') else '')
+        #    lista.append(item.get('x_studio_paqueteria',['',''])[1] if item.get('x_studio_paqueteria') else '')
+        #    lista.append(item.get('create_date'))
+        #    if item.get('x_studio_orden_general'):
+        #        lista.append("Ingreso de un producto ONE STOP sin prealerta.")
+        #    else:
+        #        lista.append("Ingreso de un producto ONE STOP.")
+                
+            #return self.create_html(datos)
+            
+        
+    #def create_html(self,lista):
+#
+    #    
+    #    html = f"""
+    #        <tr>
+    #          <td class="section" style="padding-top:12px;">
+    #            <h1 class="title">🔔 {lista[8]}</h1>
+    #            <p class="subtitle">Comunicación interna: Esta es una notificación automatica e informativa.</p>
+    #            <p class="subtitle"> Generado automáticamente el: {datetime.now().strftime("%Y-%m-%d")}</p>
+    #          </td>
+    #        </tr>
+    #        <tr>
+    #          <td class="section" style="padding-top:12px;">
+    #                <p><strong>INGRESO:</strong> {lista[1]}</p>
+    #                <p><strong>ORDEN:</strong> {lista[2]}</p>
+    #                <p><strong>PRODUCTO:</strong> {lista[3]}</p>
+    #                <p><strong>SERIE:</strong> {lista[4]}</p>
+    #                <p><strong>MODELO:</strong> {lista[5]}</p>
+    #                <p><strong>PAQUETERIA:</strong> {lista[6]}</p>
+    #                <p><strong>FECHA DE INGRESO:</strong> {lista[7]}</p>
+    #                <a href='{lista[0]}' style='background:#007BFF;
+    #                color:white;
+    #                padding:12px 20px;
+    #                text-decoration:none;
+    #                border-radius:6px;
+    #                font-family:Arial;'>
+    #                    Ver Ingreso
+    #                </a>
+    #             </td>
+    #        </tr>
+    #        
+    #    """
+    #    return html
+
+
---
+++ main.py
@@ -0,0 +1,72 @@
+import lib.email_sender as email_sender
+import ingresos as ing
+import os
+from datetime import datetime
+
+def  main():
+
+    from dotenv import load_dotenv
+    load_dotenv()
+
+    ings = ing.odoo_conection()
+    model = ings.start_odoo_connection()
+    ids = []
+    registros= ings.get_ingresos(model)
+
+    for item in registros:
+        title = ""
+        ids.append(item.get('id'))
+        url = f"https://agiotech.odoo.com//web#id={item.get('id')}&model=x_ingreso_lb&view_type=form"
+        if item.get('x_studio_orden_general'):
+            title = "Ingreso de un producto ONE STOP sin prealerta."
+        else:
+            title = "Ingreso de un producto ONE STOP."
+
+        html = f"""
+            <tr>
+              <td class="section" style="padding-top:12px;">
+                <h1 class="title">🔔 {title}</h1>
+                <p class="subtitle">Comunicación interna: Esta es una notificación automatica e informativa.</p>
+                <p class="subtitle"> Generado automáticamente el: {datetime.now().strftime("%Y-%m-%d")}</p>
+              </td>
+            </tr>
+            <tr>
+              <td class="section" style="padding-top:12px;">
+                    <p><strong>INGRESO:</strong> {item.get('x_name')}</p>
+                    <p><strong>ORDEN:</strong> {item.get('x_studio_orden_de_reparacin',['',''])[1] if item.get('x_studio_orden_de_reparacin') else ''}</p>
+                    <p><strong>PRODUCTO:</strong> {item.get('x_studio_producto',['',''])[1] if item.get('x_studio_producto') else ''}</p>
+                    <p><strong>SERIE:</strong> {item.get('x_studio_numero_de_serie_interna',['',''])[1] if item.get('x_studio_numero_de_serie_interna') else ''}</p>
+                    <p><strong>MODELO:</strong> {item.get('x_studio_modelo',['',''])[1] if item.get('x_studio_modelo') else ''}</p>
+                    <p><strong>PAQUETERIA:</strong> {item.get('x_studio_paqueteria',['',''])[1] if item.get('x_studio_paqueteria') else ''}</p>
+                    <p><strong>FECHA DE INGRESO:</strong> {item.get('create_date')}</p>
+                    <a href='{url}' style='background:#007BFF;
+                    color:white;
+                    padding:12px 20px;
+                    text-decoration:none;
+                    border-radius:6px;
+                    font-family:Arial;'>
+                        Ver Ingreso
+                    </a>
+                 </td>
+            </tr>
+            
+        """
+
+        sender = email_sender.EmailSender(
+                smtp_server = os.getenv('MAIL_SERVER'), 
+                smtp_port = os.getenv('MAIL_PORT'), 
+                username = os.getenv('MAIL_USERNAME'), 
+                password = os.getenv('MAIL_PASSWORD'), use_tls=True)
+        
+        template = sender.mail_template(title="Notificacion ONE STOP", rows=html)
+
+        sender.send_html_email(
+            to_email="isaias.avila@agiotech.com",
+            subject="Notificacion ONE STOP",
+            html_content=template,
+            from_email=os.getenv('MAIL_FROM')
+        )
+        
+
+if __name__ == "__main__":
+    main()
